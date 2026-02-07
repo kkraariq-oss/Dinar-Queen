837e2612c7cee0d9846573980fe9e9874d0b5bdd
kkraariq-oss <kkraariq@gmail.com>
2026-02-07 20:51:15 +0300

    Replace Supabase anon key with a new value
    Updated Supabase anon key for secure access.

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ if ('serviceWorker' in navigator) {
 }
 
 // إعدادات Supabase
-const SUPABASE_URL = 'https://umlbxdcgpdifxzijujvj.supabase.co
-';
-const SUPABASE_ANON_KEY = 'sb_publishable_H9UQUj6cvu5kdzJAuIJj9Q_vO8bqu';
+const SUPABASE_URL = 'https://umlbxdcgpdifxzijujvj.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtbGJ4ZGNncGRpZnh6aWp1anZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0NzQzODUsImV4cCI6MjA4NjA1MDM4NX0.Ld3fU2_B4eu803BsDYKQ0ofg69WxQPJcscGf93lnM3w';
 
 // إنشاء عميل Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
