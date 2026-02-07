feae52adcee248f8b1626a35defe153cd891e45d
kkraariq-oss <kkraariq@gmail.com>
2026-02-07 22:34:46 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ if ('serviceWorker' in navigator) {
 }
 
 // إعدادات Supabase
-const SUPABASE_URL = 'https://umlbxdcgpdifxzijujvj.supabase.co';
+const SUPABASE_URL = 'https://yusbdsss8rgrgpwv5gl71da.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtbGJ4ZGNncGRpZnh6aWp1anZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0NzQzODUsImV4cCI6MjA4NjA1MDM4NX0.Ld3fU2_B4eu803BsDYKQ0ofg69WxQPJcscGf93lnM3w';
 
 // إنشاء عميل Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+var supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 let currentUser = null;
 let userDataListener = null;
